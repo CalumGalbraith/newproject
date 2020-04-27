b8c070cb2429e574d89d2668f24e29254abd3b18
Calum Galbraith <info@calumgalbraith.co.uk>
2020-04-27 17:20:33 +0100

    Fixed typos, changed file population to multi-line string and set Flask ENV

--- newproject.py
+++ newproject.py
@@ -24,15 +24,24 @@ def start() :
         filepath = Path(f"{name}/app.py")
 
         with filepath.open("a") as f:
-            f.write('from flask import Flask, render_template \n')
-            f.write('app = Flask(__name__) \n')
-            f.write(' /n')
-            f.write('@app.route("/") \n')
-            f.write(' /n')
-            f.write('def index(): \n')
-            f.write('\treturn render_template ("index.html")')
+            f.write("""from flask import Flask, render_template
+            app = Flask(__name__)
+
+            @app.route("/")
+            def index():
+                return render_template ("index.html")""")
             f.close()
             print ("app.py Created")
+
+        Path(f"{name}/#.flaskenv").touch()
+        filepath = Path(f"{name}/#.flaskenv")
+
+        with filepath.open("a") as f:
+            f.write("""FLASK_APP=app.py
+FLASK_ENV=development""")
+            f.close()
+            print ("Flask Environment set")
+
         # Creates templates directory inside new user directory
         Path(f"{name}/templates").mkdir(parents=True, exist_ok=True)
         print ("Templates directory created")
@@ -43,39 +52,44 @@ def start() :
 
         # Creates index.html in templates directory
         Path(f"{name}/static/style.css").touch()
+        print ("style.css created")
 
         # Creates index.html in templates directory
         Path(f"{name}/templates/index.html").touch()
         filepath = Path(f"{name}/templates/index.html")
 
         with filepath.open("a") as f:
-            f.write('{% extends "layout.html" %} \n')
-            f.write('{% block heading %} \n')
-            f.write(' \n')
-            f.write('{% endblock %} \n')
-            f.write(' \n')
-            f.write('{% body block %} \n')
-            f.write(' \n')
-            f.write('{% endblock %} \n')
-        print ("index.html created")
+            f.write("""{% extends "layout.html" %}
+
+{% block heading %}
+
+{% endblock %}
+
+{% block body %}
+
+{% endblock %}""")
+            f.close()
+            print ("index.html created")
 
         # Creates layout.html in templates directory
         Path(f"{name}/templates/layout.html").touch()
         filepath = Path(f"{name}/templates/layout.html")
 
         with filepath.open("a") as f:
-            f.write('<!DOCTYPE html> \n')
-            f.write('<html> \n')
-            f.write('\t<head> \n')
-            f.write('\t<link rel="stylesheet" href="static/styles.css"> \n')
-            f.write('\t\t<title> </title> \n')
-            f.write('\t</head> \n')
-            f.write('\t<body>\n')
-            f.write('\t\t<h1>{% block heading %}{% endblock %}</h1> \n')
-            f.write('\t\t{% block body %} \n')
-            f.write('\t\t{% endblock %} \n')
-            f.write('\t</body>\n')
-            f.write('</html>')
+            f.write(""" <!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="static/style.css">
+        <title> </title>
+    </head>
+    <body>
+        <h1>{% block heading %}{% endblock %}</h1>
+
+        {% block body %}
+        {% endblock %}
+
+    </body>
+</html>""")
             f.close()
             print ("layout.html created")
             print ("Setup Complete")
