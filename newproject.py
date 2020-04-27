29247caa4764c753b59aa7cacc3a30f8b50d9e22
Calum Galbraith <info@calumgalbraith.co.uk>
2020-04-27 14:28:46 +0100

    removed os module in place of pathlib and added file writing

--- newproject.py
+++ newproject.py
@@ -1,35 +1,87 @@
 from pathlib import Path
-import os
+import sys
 
-#Askes User for folder name
-name = input("Enter the Project Name: ")
+invalid_input = True
 
-# Check to see if the already directory exsists
-# Set directory to user input
-path = f"./{name}"
+def start() :
+    print("Start your new project")
+    name = input("Enter the Project Name: ")
+    directory = Path(f"./{name}")
 
-# Check whether the specified path exists
-isExist = os.path.exists(path)
+    # If the path does exist run the following
+    if directory.is_dir():
+        print ("The directory already exists")
 
-# If it exists return this error
-if (isExist) == True:
-    print("The directory already exists")
-    
-# If the directory does not exist run this script
-else:
-# Creates directory from user input
-    os.mkdir(f"{name}")
+    else:
+        # Set to False because input was valid
+        invalid_input = False
 
-    os.path.dirname(f"{name}")
+        # Creates directory from user input
+        Path(f"./{name}").mkdir(parents=True, exist_ok=True)
+        print ("Project Directory Created")
+        # Creates app.py in new user directory
+        Path(f"{name}/app.py").touch()
+        filepath = Path(f"{name}/app.py")
 
-# Creates app.py in new user directory
-    Path(f"{name}/app.py").touch()
+        with filepath.open("a") as f:
+            f.write('from flask import Flask, render_template \n')
+            f.write('app = Flask(__name__) \n')
+            f.write(' /n')
+            f.write('@app.route("/") \n')
+            f.write(' /n')
+            f.write('def index(): \n')
+            f.write('\treturn render_template ("index.html")')
+            f.close()
+            print ("app.py Created")
+        # Creates templates directory inside new user directory
+        Path(f"{name}/templates").mkdir(parents=True, exist_ok=True)
+        print ("Templates directory created")
 
-# Creates templates directory inside new user directory
-    os.mkdir(f"{name}/templates")
+        # Creates static directory inside new user directory
+        Path(f"{name}/static").mkdir(parents=True, exist_ok=True)
+        print ("Static directory created")
 
-# Creates index.html in templates directory
-    Path(f"{name}/templates/index.html").touch()
+        # Creates index.html in templates directory
+        Path(f"{name}/static/style.css").touch()
 
-# Creates layout.html in templates directory
-    Path(f"{name}/templates/layout.html").touch()
+        # Creates index.html in templates directory
+        Path(f"{name}/templates/index.html").touch()
+        filepath = Path(f"{name}/templates/index.html")
+
+        with filepath.open("a") as f:
+            f.write('{% extends "layout.html" %} \n')
+            f.write('{% block heading %} \n')
+            f.write(' \n')
+            f.write('{% endblock %} \n')
+            f.write(' \n')
+            f.write('{% body block %} \n')
+            f.write(' \n')
+            f.write('{% endblock %} \n')
+        print ("index.html created")
+
+        # Creates layout.html in templates directory
+        Path(f"{name}/templates/layout.html").touch()
+        filepath = Path(f"{name}/templates/layout.html")
+
+        with filepath.open("a") as f:
+            f.write('<!DOCTYPE html> \n')
+            f.write('<html> \n')
+            f.write('\t<head> \n')
+            f.write('\t<link rel="stylesheet" href="static/styles.css"> \n')
+            f.write('\t\t<title> </title> \n')
+            f.write('\t</head> \n')
+            f.write('\t<body>\n')
+            f.write('\t\t<h1>{% block heading %}{% endblock %}</h1> \n')
+            f.write('\t\t{% block body %} \n')
+            f.write('\t\t{% endblock %} \n')
+            f.write('\t</body>\n')
+            f.write('</html>')
+            f.close()
+            print ("layout.html created")
+            print ("Setup Complete")
+
+        sys.exit()
+
+# this will loop until user enters a valid directory
+while invalid_input :
+    start()
